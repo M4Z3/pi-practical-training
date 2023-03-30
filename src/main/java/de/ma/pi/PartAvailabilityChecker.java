910c12986286510fe0815eb59bcd937ec5c33ee0
Mathias Wanzenberg <kaeferhaufen@gmail.com>
2023-03-30 17:47:22 -0400

    add practical training bpmn with png to quick view

---
+++ src/main/java/de/ma/pi/PartAvailabilityChecker.java
@@ -0,0 +1,11 @@
+package de.ma.pi;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class PartAvailabilityChecker  implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+    }
+}
