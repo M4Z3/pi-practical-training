16ce6c7d2094ad80c1f9b01d9add9a17f2d7193b
Janik Dohrmann <janik.dohrmann@gmx.de>
2023-04-20 20:45:12 +0200

    Basic automation flow

--- src/main/java/de/ma/pi/automation/APIConnector.java
+++ src/main/java/de/ma/pi/automation/APIConnector.java
@@ -11,6 +11,7 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class APIConnector {
     private String baseUrl;
@@ -18,12 +19,16 @@ public class APIConnector {
         this.baseUrl = baseUrl;
     }
 
+    public static final String[] deviceTypes = new String[]{"available_pc", "out_of_stock_pc", "not_functional_pc"};
+
     public String start() {
+        Random random = new Random();
+
         try {
             String body = "{\n" +
                     "  \"messageName\" : \"Message_start_personal_computer_sale\",\n" +
                     "  \"processVariables\" : {\n" +
-                    "    \"device_type\" : {\"value\" : \"available_pc\", \"type\": \"String\"}\n" +
+                    "    \"device_type\" : {\"value\" : \"" + deviceTypes[random.nextInt(3)] + "\", \"type\": \"String\"}\n" +
                     "  }\n" +
                     "}";
 
@@ -43,11 +48,10 @@ public class APIConnector {
         }
     }
 
-    public String getPendingUsertaskCallPresentOffer() {
+    public List<ProcessInstance> getPendingUsertaskCallPresentOffer() {
         try {
             HttpResponse<String> response = httpGetRequest("/task?taskDefinitionKey=Usertask_call_present_offer");
-            return response.body();
-            //return new ArrayList<>(Arrays.asList(new Gson().fromJson(response.body(), ProcessInstance[].class)));
+            return new ArrayList<>(Arrays.asList(new Gson().fromJson(response.body(), ProcessInstance[].class)));
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -64,27 +68,56 @@ public class APIConnector {
 
             HttpResponse<String> response = httpPostRequest(String.format("/task/%s/submit-form", id), body);
             return response.body();
-            //return new ArrayList<>(Arrays.asList(new Gson().fromJson(response.body(), ProcessInstance[].class)));
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String getPendingUsertaskCallAlternativeParts() {
+    public List<ProcessInstance> getPendingUsertaskCallAlternativeParts() {
         try {
             HttpResponse<String> response = httpGetRequest("/task?taskDefinitionKey=Usertask_call_alternative_parts");
-            return response.body();
-            //return new ArrayList<>(Arrays.asList(new Gson().fromJson(response.body(), ProcessInstance[].class)));
+            return new ArrayList<>(Arrays.asList(new Gson().fromJson(response.body(), ProcessInstance[].class)));
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String getPendingUsertaskCheckPayment() {
+    public String completeUsertaskCallAlternativeParts(String id) {
+        Random random = new Random();
+        try {
+            String body = "{\n" +
+                    "  \"variables\": {\n" +
+                    "    \"order_cancelled\": {\"value\":\"answer_no\",\"type\":\"String\"},\n" +
+                    "    \"new_device_type\": {\"value\":\"" + deviceTypes[random.nextInt(3)] + "\",\"type\":\"String\"}\n" +
+                    "  }\n" +
+                    "}";
+
+            HttpResponse<String> response = httpPostRequest(String.format("/task/%s/submit-form", id), body);
+            return response.body();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<ProcessInstance> getPendingUsertaskCheckPayment() {
         try {
             HttpResponse<String> response = httpGetRequest("/task?taskDefinitionKey=Usertask_check_payment");
+            return new ArrayList<>(Arrays.asList(new Gson().fromJson(response.body(), ProcessInstance[].class)));
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String completeUsertaskCheckPayment(String id) {
+        try {
+            String body = "{\n" +
+                    "  \"variables\": {\n" +
+                    "    \"customer_paid\": {\"value\":\"answer_yes\",\"type\":\"String\"}\n" +
+                    "  }\n" +
+                    "}";
+
+            HttpResponse<String> response = httpPostRequest(String.format("/task/%s/submit-form", id), body);
             return response.body();
-            //return new ArrayList<>(Arrays.asList(new Gson().fromJson(response.body(), ProcessInstance[].class)));
         } catch (URISyntaxException | IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/de/ma/pi/automation/Main.java
+++ src/main/java/de/ma/pi/automation/Main.java
@@ -1,14 +1,34 @@
 package de.ma.pi.automation;
 
+import java.util.List;
+
 public class Main {
+
+    public static final int simulations = 1000;
+
     public static void main(String[] args) {
         APIConnector apiConnector = new APIConnector("http://localhost:8080/engine-rest");
 
-        System.out.println(apiConnector.start());
-        System.out.println(apiConnector.getActive());
-        System.out.println(apiConnector.getPendingUsertaskCallPresentOffer());
-        System.out.println(apiConnector.completeUsertaskCallPresentOffer("e688dc88-de8c-11ed-a8a3-6c6a773c918d"));
-        System.out.println(apiConnector.getPendingUsertaskCallAlternativeParts());
-        System.out.println(apiConnector.getPendingUsertaskCheckPayment());
+        for(int i = 0; i < simulations; i++) {
+            apiConnector.start();
+
+            List<ProcessInstance> listPendingUsertaskCallPresentOffer = apiConnector.getPendingUsertaskCallPresentOffer();
+
+            for (ProcessInstance instance: listPendingUsertaskCallPresentOffer) {
+                apiConnector.completeUsertaskCallPresentOffer(instance.getId());
+            }
+
+            List<ProcessInstance> listPendingUsertaskCallAlternativeParts = apiConnector.getPendingUsertaskCallAlternativeParts();
+
+            for (ProcessInstance instance: listPendingUsertaskCallAlternativeParts) {
+                apiConnector.completeUsertaskCallAlternativeParts(instance.getId());
+            }
+
+            List<ProcessInstance> listPendingUsertaskCheckPayment = apiConnector.getPendingUsertaskCheckPayment();
+
+            for (ProcessInstance instance: listPendingUsertaskCheckPayment) {
+                apiConnector.completeUsertaskCheckPayment(instance.getId());
+            }
+        }
     }
 }
